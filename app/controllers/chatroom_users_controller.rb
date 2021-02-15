4543067a7c4c1eca67123eaae1c7f131c167e6bf
crystalrain125 <cxu1169@gmail.com>
2021-02-14 17:53:24 -0800

    Create groups controllers and models.

---
+++ app/controllers/chatroom_users_controller.rb
@@ -0,0 +1,42 @@
+class ChatroomUsersController < ApplicationController
+
+
+	def create
+		@chatroom = Chatroom.find(params[:chatroom_id])
+		@request = Request.find(params[:request_id])
+		@chatroom_user = @chatroom.chatroom_users.where(user_id: @request.user_id).first_or_create
+		
+		@chatroom.requests.where(user_id: @request.user_id).destroy_all
+		redirect_to chatrooms_path(:uid => current_user.uid, :id => current_user.id)
+	end 
+	
+
+	
+	def show
+		@chatroom = Chatroom.find(params[:chatroom_id])
+		@chatroom_user = @chatroom.chatroom_users.where(user_id: current_user.id).first_or_create
+		@chatroom_user.update_attribute(:admin, true)
+	
+		redirect_to @chatroom
+		
+		
+	end 
+	
+	def destroy
+		@chatroom = Chatroom.find(params[:chatroom_id])
+		@chatroom_user = @chatroom.chatroom_users.where(user_id: current_user.id).first
+		@chatroom.chatroom_users.where(user_id: current_user.id).destroy_all
+		if @chatroom_user.admin == true
+			@chatroom.chatroom_users.each do |x|
+				x.update_attribute(:admin, true)
+			end
+		end
+		redirect_to chatrooms_path(:uid => current_user.uid, :id => current_user.id)
+	end
+	
+
+	
+
+
+
+end
---
+++ app/models/chatroom.rb
@@ -0,0 +1,13 @@
+class Chatroom < ApplicationRecord
+	has_many :chatroom_users
+	has_many :users, through: :chatroom_users
+	has_many :requests
+
+
+    validates :title, presence: true
+    
+    searchable do
+        text :details
+        text :title
+    end
+end

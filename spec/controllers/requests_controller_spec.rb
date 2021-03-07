43282d248d10ade56dcad79177121206d4736d81
Simon <76978805+5imyboy@users.noreply.github.com>
2021-03-07 14:09:02 -0800

    Tests (#31)
    * test cases
    
    Test cases for controller classes
    
    * Test syntax
    
    updated syntax to satisfy rubocop linter
    
    * Revert "Merge branch 'master' into tests"
    
    This reverts commit 3a511ac9950ec0ffa47652d0199e897d5157982b, reversing
    changes made to 72058b34e5da0cd479384768d2bc00ab05f4f415.
    
    Co-authored-by: Simon Yong <simyboy@g.ucla.edu>

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -1,27 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe RequestsController, :type => :controller do
-    
-    describe '#create' do
-        it 'successful creation of request' do
-            req = create(:request)
-            expect(req).to be_valid
-        end
+RSpec.describe RequestsController, type: 'controller' do
+  describe '#create' do
+    it 'successful creation of request' do
+      req = create(:request)
+      expect(req).to be_valid
     end
-    
-    """
-    describe '#destroy' do
-        let(:request) { FactoryBot.create(:request) }
-        before do
-            request
-        end
-        it 'successful deletion of request' do
-            #req = create(:request)
-            #delete :destroy_all, :id request
-            expect { delete :destroy_all, params: {:id => request.id} }.to_not change(Request, :count)
-            #expect { delete :destroy_all, :id request }.to change(Request, :count).by(-1)
-        end
-    end
-    """
-    
+  end
 end
